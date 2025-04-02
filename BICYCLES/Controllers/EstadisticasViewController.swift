940302d5cbcbf5b9259b3d6571321f1ca43e65a2
Redaser <hugormzvillarreal@gmail.com>
2025-04-02 10:21:42 -0600

    Grafica jalando
    grafica jalando

--- BICYCLES/Controllers/EstadisticasViewController.swift
+++ BICYCLES/Controllers/EstadisticasViewController.swift
@@ -15,17 +15,20 @@ class EstadisticasViewController: UIViewController {
     @IBOutlet weak var lblMejor: UILabel!
     @IBOutlet weak var lblPromedio: UILabel!
     @IBOutlet weak var vwContenedorGrafica: UIView!
-    var generales:[String: Any] = [:]
-    var datos:[String: Any] = [:]
+    var generales:[String: [String: Double]] = [:]
+    var datos:[String: [Double]] = [:]
     let graficaBarras: BarChartView = {
         let chart = BarChartView()
 
+        chart.viewPortHandler.setMaximumScaleX(2.0) // Ajusta la escala X
+        chart.viewPortHandler.setMaximumScaleY(2.0) // Ajusta la escala Y
         chart.chartDescription.enabled = false
         chart.rightAxis.enabled = false
         chart.leftAxis.enabled = false
         chart.legend.enabled = true
 
         chart.drawValueAboveBarEnabled = true
+        chart.autoScaleMinMaxEnabled = true
         
         return chart
     }()
@@ -41,18 +44,15 @@ class EstadisticasViewController: UIViewController {
             make.leading.equalToSuperview().inset(8)
             make.trailing.equalToSuperview().inset(8)
             make.bottom.equalToSuperview().inset(8)
+            make.height.equalTo(300)
         }
-
-        
         peticionApi()
-        configurarDataGrafica()
 
         
     }
-    
+        
     @IBAction func cambioValorSegmented(_ sender: UISegmentedControl) {
         
-        print(sender.selectedSegmentIndex)
         var seccion: String = ""
         switch sender.selectedSegmentIndex {
             case 0:
@@ -63,12 +63,13 @@ class EstadisticasViewController: UIViewController {
             seccion = "calorias"
         }
         
+        configurarDataGrafica(seccion)
 
     }
     
     func peticionApi() {
 
-        if let url = URL(string: "http://192.168.252.110:8000/api/v1/semana/estadisticas") {
+        if let url = URL(string: "http://192.168.252.212:8000/api/v1/semana/estadisticas") {
             guard let token = SessionManager.shared.getToken() else {
                 return
             }
@@ -95,9 +96,11 @@ class EstadisticasViewController: UIViewController {
                 if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
                     do {
                         if let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                            //print("Datos recibidos: \(jsonResponse)")
-                            self.generales = jsonResponse["generales"] as! [String: Any]
-                            self.datos = jsonResponse["data"] as! [String: Any]
+                            print("Datos recibidos: \(jsonResponse)")
+                            self.generales = jsonResponse["generales"] as! [String: [String: Double]]
+                            self.datos = jsonResponse["data"] as! [String: [Double]]
+                            
+                            self.configurarDataGrafica()
                             
                         } else {
                             print("Formato de respuesta no válido.")
@@ -119,7 +122,14 @@ class EstadisticasViewController: UIViewController {
     
     func configurarDataGrafica(_ seccion: String = "calorias"){
         
-        let valores: [Double] = [50, 104, 81, 93, 52, 100, 500]
+        DispatchQueue.main.async {
+            self.lblMejor.text = String(self.generales[seccion]!["maxima"]!)
+            self.lblTotal.text = String(self.generales[seccion]!["total"]!)
+            self.lblPromedio.text = String(self.generales[seccion]!["promedio"]!)
+        }
+        
+        
+        let valores: [Double] = datos[seccion]!
         var entradasDeDatos: [BarChartDataEntry] = []
         
 
@@ -137,11 +147,6 @@ class EstadisticasViewController: UIViewController {
         
         let datos = BarChartData(dataSet: conjuntoDeDatos)
         graficaBarras.data = datos
-        
-        if let maximoY = valores.max() {
-            graficaBarras.leftAxis.axisMaximum = maximoY + 10
-            graficaBarras.leftAxis.axisMinimum = 0
-        }
         
         graficaBarras.xAxis.valueFormatter = IndexAxisValueFormatter(values: ["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"])
         graficaBarras.xAxis.granularity = 1
